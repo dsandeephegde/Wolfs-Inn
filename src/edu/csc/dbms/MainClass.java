6e8c2522c2dee3bdf9fa6853a295fb57b9c75bbe
AnkithRD <rdankith@gmail.com>
2018-04-10 16:41:37 -0400

    Hotels U

--- src/edu/csc/dbms/MainClass.java
+++ src/edu/csc/dbms/MainClass.java
@@ -23,20 +23,31 @@ public class MainClass {
 				
 				switch(option) {
 				
-				case 1:
+				case 1://Show All Hotels
 					mc.showHotels();
 					break;
 					
-				case 2: 
+				case 2://Add new Hotel
 					mc.addHotel(scan);
 					mc.showHotels();
 					break;
 					
-				case 3:
+				case 3://Delete Hotel
 					mc.showHotels();
 					mc.deleteHotelById(scan);
 					mc.showHotels();
 					break;
+				
+				case 4://Update Hotel
+					mc.showHotels();
+					mc.updateHotel(scan);
+					mc.showHotels();
+					break;
+					
+				case 5://Show All Staffs
+					mc.showStaffs();
+					break;
+						
 					
 				default:
 					System.out.println("case default");
@@ -62,6 +73,11 @@ public class MainClass {
 		System.out.println("1. List all Hotels");
 		System.out.println("2. Add new Hotel");
 		System.out.println("3. Delete existing Hotel");
+		System.out.println("4. Update existing Hotel");
+		System.out.println("5. List All Staff");
+		System.out.println("6. Add new Staff");
+		System.out.println("7. Delete existing Staff");
+		System.out.println("8. Update existing Staff");
 		System.out.println("------------------------------");
 	}
 	
@@ -158,5 +174,79 @@ public class MainClass {
 		String query = "Delete from hotels where " + NS.HOTELS_ID +" = " + hotelId;
 		getResult(query);
 	}
+	
+	private void updateHotel(Scanner scan) {
+		
+		System.out.println("Enter hotel ID to update : ");
+		String hotelId = scan.nextLine();
+		
+		String updateString = new String();
+		
+		System.out.println("Enter only update values");
+		System.out.println("Enter hotel name : ");
+		String name = scan.nextLine();
+		
+		if(!name.isEmpty())
+			updateString += NS.HOTELS_NAME + " = '" +  name + "'";
+		
+		System.out.println("Enter line 1 address : ");
+		String address = scan.nextLine();
+		
+		if(!address.isEmpty()) {
+			updateString += (!updateString.isEmpty())? " , ":"" ; 
+			updateString += NS.HOTELS_ADDRESS + " = '" +  address + "'";
+		}
+			
+		
+		System.out.println("Enter City : ");
+		String city = scan.nextLine();
+		
+		if(!city.isEmpty()) {
+			updateString += (!updateString.isEmpty())? " , ":"" ; 
+			updateString += NS.HOTELS_CITY + " = '" +  city + "'";
+		}
+		
+		System.out.println("Enter State : ");
+		String state = scan.nextLine();
+		
+		if(!state.isEmpty()) {
+			updateString += (!updateString.isEmpty())? " , ":"" ; 
+			updateString += NS.HOTELS_STATE + " = '" +  state + "'";
+		}
+		
+		System.out.println("Enter Country : ");
+		String country = scan.nextLine();
+		
+		if(!country.isEmpty()) {
+			updateString += (!updateString.isEmpty())? " , ":"" ; 
+			updateString += NS.HOTELS_COUNTRY + " = '" +  country + "'";
+		}
+		
+		System.out.println("Enter phone number : ");
+		String phNum = scan.nextLine();
+		
+		if(!country.isEmpty()) {
+			updateString += (!updateString.isEmpty())? " , ":"" ; 
+			updateString += NS.HOTELS_PH_NUMBER + " = '" +  phNum + "'";
+		}
+		
+		System.out.println("Enter Manager ID : ");
+		String managerId = scan.nextLine();
+		
+		if(!country.isEmpty()) {
+			updateString += (!updateString.isEmpty())? " , ":"" ; 
+			updateString += NS.HOTELS_MANAGER_ID + " = " +  managerId + "";
+		}
+		
+		String query = "Update hotels set " + updateString + " where " + NS.HOTELS_ID + " = " + hotelId;
+		getResult(query);
+		
+	}
+
+	private void showStaffs() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
